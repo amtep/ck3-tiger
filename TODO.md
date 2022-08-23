4777a3fe9d52f04d69cd475cc8bedf3c53688c15
Richard Braakman <rebraakman@gmail.com>
2022-08-23 21:11:51 +0300

    Add a few more tests

--- TODO.md
+++ TODO.md
@@ -5,6 +5,7 @@
 ### Display
 
 * The caret placement for error messages assumes 1 byte of the source string = 1 space, which is inaccurate both for tabs and for multibyte characters.
+* The column numbers reported for pdxscript files are wrong.
 
 ## Features
 
--- tests/test.rs
+++ tests/test.rs
@@ -12,7 +12,7 @@ fn test_mod_1() {
     set_mod_root(mod_root.clone());
     log_to(Box::new(Vec::new()));
 
-    let mut everything = Everything::new(vanilla_root, mod_root).unwrap();
+    let mut everything = Everything::new(&vanilla_root, &mod_root).unwrap();
     everything.load_all();
     everything.check_all();
 
@@ -22,4 +22,27 @@ fn test_mod_1() {
     // TODO: check for absence of duplicate event warning for non-dup.0001
 
     assert!(errors.contains("bad_loca_name.yml: ERROR: could not determine language from filename"));
+
+    assert!(
+        errors.contains("decision.txt:2:12: ERROR: missing english localization key my_decision ")
+    );
+    assert!(errors.contains(
+        "decision.txt:2:12: ERROR: missing english localization key my_decision_tooltip "
+    ));
+    assert!(errors
+        .contains("decision.txt:2:12: ERROR: missing english localization key my_decision_desc "));
+    assert!(errors.contains(
+        "decision.txt:2:12: ERROR: missing english localization key my_decision_confirm "
+    ));
+
+    assert!(errors
+        .contains("decision.txt:8:31: ERROR: missing english localization key my_decision_also "));
+    assert!(errors.contains(
+        "decision.txt:9:38: ERROR: missing english localization key my_decision2_description "
+    ));
+    assert!(errors.contains(
+        "decision.txt:10:44: ERROR: missing english localization key totally_different "
+    ));
+    assert!(errors
+        .contains("decision.txt:11:36: ERROR: missing english localization key my_decision2_c "));
 }
