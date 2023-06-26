35974dcf21ee1db470676d776c34bbf26cc2eae2
Richard Braakman <rebraakman@gmail.com>
2023-06-26 19:38:41 +0300

    add missing file

---
+++ src/data/coadesigner.rs
@@ -0,0 +1,103 @@
+use crate::block::validator::Validator;
+use crate::block::Block;
+use crate::data::coa::validate_coa_layout;
+use crate::db::{Db, DbKind};
+use crate::errorkey::ErrorKey;
+use crate::errors::warn;
+use crate::everything::Everything;
+use crate::item::Item;
+use crate::token::Token;
+
+#[derive(Clone, Debug)]
+pub struct CoaDesignerColoredEmblem {}
+
+impl CoaDesignerColoredEmblem {
+    pub fn add(db: &mut Db, key: Token, block: Block) {
+        db.add(
+            Item::CoaDesignerColoredEmblem,
+            key,
+            block,
+            Box::new(Self {}),
+        );
+    }
+}
+
+impl DbKind for CoaDesignerColoredEmblem {
+    fn validate(&self, key: &Token, block: &Block, data: &Everything) {
+        let mut vd = Validator::new(block, data);
+
+        let pathname = format!("{}{key}", Item::CoaDesignerColoredEmblem.path());
+        data.verify_exists_implied(Item::File, &pathname, key);
+
+        vd.field_integer_range("colors", 0, 5);
+        vd.field_bool("visible");
+        if let Some(token) = vd.field_value("category") {
+            let loca = format!("COA_DESIGNER_CATEGORY_{token}");
+            data.verify_exists_implied(Item::Localization, &loca, token);
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CoaDesignerColorPalette {}
+
+impl CoaDesignerColorPalette {
+    pub fn add(db: &mut Db, key: Token, block: Block) {
+        if key.is("coa_designer_background_colors") {
+            for (token, block) in block.iter_pure_definitions_warn() {
+                db.add(
+                    Item::CoaDesignerColorPalette,
+                    token.clone(),
+                    block.clone(),
+                    Box::new(Self {}),
+                );
+            }
+        } else {
+            warn(key, ErrorKey::UnknownField, "unknown key");
+        }
+    }
+}
+
+impl DbKind for CoaDesignerColorPalette {
+    fn validate(&self, key: &Token, block: &Block, data: &Everything) {
+        let mut _vd = Validator::new(block, data);
+
+        data.verify_exists(Item::NamedColor, key);
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CoaDesignerEmblemLayout {}
+
+impl CoaDesignerEmblemLayout {
+    pub fn add(db: &mut Db, key: Token, block: Block) {
+        db.add(Item::CoaDesignerEmblemLayout, key, block, Box::new(Self {}));
+    }
+}
+
+impl DbKind for CoaDesignerEmblemLayout {
+    fn validate(&self, _key: &Token, block: &Block, data: &Everything) {
+        validate_coa_layout(block, data);
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CoaDesignerPattern {}
+
+impl CoaDesignerPattern {
+    pub fn add(db: &mut Db, key: Token, block: Block) {
+        db.add(Item::CoaDesignerPattern, key, block, Box::new(Self {}));
+    }
+}
+
+impl DbKind for CoaDesignerPattern {
+    fn validate(&self, key: &Token, block: &Block, data: &Everything) {
+        let mut vd = Validator::new(block, data);
+
+        let pathname = format!("{}{key}", Item::CoaDesignerPattern.path());
+        data.verify_exists_implied(Item::File, &pathname, key);
+
+        vd.field_integer_range("colors", 0, 5);
+        vd.field_bool("visible");
+    }
+}
