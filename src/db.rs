7261c9e5c7f8b3add4d6c5f59c8f708be077f57b
Richard Braakman <shuttlegit@gmail.com>
2023-07-15 15:23:25 +0300

    Refactor Db to have a vector as its first layer

--- src/db.rs
+++ src/db.rs
@@ -17,20 +17,19 @@ use crate::token::Token;
 #[derive(Debug)]
 pub struct Db {
     /// Items with full DbEntries, meaning a key and a block for each.
-    /// For convenience, the secondary hashmap is pre-made for every Item variant
-    database: FnvHashMap<Item, FnvHashMap<String, DbEntry>>,
+    /// The `Vec` is indexed with an `Item` discriminant.
+    database: Vec<FnvHashMap<String, DbEntry>>,
     /// Items generated as side effects of the full items in `database`.
-    /// For convenience, the secondary hashmap is pre-made for every Item variant
-    flags: FnvHashMap<Item, FnvHashMap<String, Token>>,
+    /// The `Vec` is indexed with an `Item` discriminant.
+    flags: Vec<FnvHashMap<String, Token>>,
 }
-// TODO: make the upper level of the Db use a Vec rather than a HashMap
 
 impl Default for Db {
     fn default() -> Self {
-        let mut db = Self { database: FnvHashMap::default(), flags: FnvHashMap::default() };
-        for itype in Item::iter() {
-            db.database.insert(itype, FnvHashMap::default());
-            db.flags.insert(itype, FnvHashMap::default());
+        let mut db = Self { database: Vec::default(), flags: Vec::default() };
+        for _ in Item::iter() {
+            db.database.push(FnvHashMap::default());
+            db.flags.push(FnvHashMap::default());
         }
         db
     }
@@ -38,7 +37,7 @@ impl Default for Db {
 
 impl Db {
     pub fn add(&mut self, item: Item, key: Token, block: Block, kind: Box<dyn DbKind>) {
-        if let Some(other) = self.database.get(&item).unwrap().get(key.as_str()) {
+        if let Some(other) = self.database[item as usize].get(key.as_str()) {
             if other.key.loc.kind >= key.loc.kind {
                 if other.block.equivalent(&block) {
                     exact_dup_error(&key, &other.key, &item.to_string());
@@ -47,7 +46,7 @@ impl Db {
                 }
             }
         }
-        self.database.get_mut(&item).unwrap().insert(key.to_string(), DbEntry { key, block, kind });
+        self.database[item as usize].insert(key.to_string(), DbEntry { key, block, kind });
     }
 
     pub fn add_exact_dup_ok(
@@ -57,7 +56,7 @@ impl Db {
         block: Block,
         kind: Box<dyn DbKind>,
     ) {
-        if let Some(other) = self.database.get(&item).unwrap().get(key.as_str()) {
+        if let Some(other) = self.database[item as usize].get(key.as_str()) {
             if other.key.loc.kind >= key.loc.kind {
                 if other.block.equivalent(&block) {
                     dup_advice(&key, &other.key, &item.to_string());
@@ -66,15 +65,15 @@ impl Db {
                 }
             }
         }
-        self.database.get_mut(&item).unwrap().insert(key.to_string(), DbEntry { key, block, kind });
+        self.database[item as usize].insert(key.to_string(), DbEntry { key, block, kind });
     }
 
     pub fn add_flag(&mut self, item: Item, key: Token) {
-        self.flags.get_mut(&item).unwrap().insert(key.to_string(), key);
+        self.flags[item as usize].insert(key.to_string(), key);
     }
 
     pub fn validate(&self, data: &Everything) {
-        self.database.par_iter().for_each(|(_, hash)| {
+        self.database.par_iter().for_each(|hash| {
             hash.par_iter().for_each(|(_, entry)| {
                 entry.kind.validate(&entry.key, &entry.block, data);
             });
@@ -82,12 +81,12 @@ impl Db {
     }
 
     pub fn exists(&self, item: Item, key: &str) -> bool {
-        self.database.get(&item).unwrap().contains_key(key)
-            || self.flags.get(&item).unwrap().contains_key(key)
+        self.database[item as usize].contains_key(key)
+            || self.flags[item as usize].contains_key(key)
     }
 
     pub fn get_item<T: DbKind + Any>(&self, item: Item, key: &str) -> Option<(&Token, &Block, &T)> {
-        if let Some(entry) = self.database.get(&item).unwrap().get(key) {
+        if let Some(entry) = self.database[item as usize].get(key) {
             if let Some(kind) = (*entry.kind).as_any().downcast_ref::<T>() {
                 return Some((&entry.key, &entry.block, kind));
             }
@@ -96,11 +95,11 @@ impl Db {
     }
 
     pub fn get_key_block(&self, item: Item, key: &str) -> Option<(&Token, &Block)> {
-        self.database.get(&item).unwrap().get(key).map(|entry| (&entry.key, &entry.block))
+        self.database[item as usize].get(key).map(|entry| (&entry.key, &entry.block))
     }
 
     pub fn has_property(&self, item: Item, key: &str, property: &str, data: &Everything) -> bool {
-        if let Some(entry) = self.database.get(&item).unwrap().get(key) {
+        if let Some(entry) = self.database[item as usize].get(key) {
             entry.kind.has_property(&entry.key, &entry.block, property, data)
         } else {
             false
@@ -108,7 +107,7 @@ impl Db {
     }
 
     pub fn set_property(&mut self, item: Item, key: &str, property: &str) {
-        if let Some(entry) = self.database.get_mut(&item).unwrap().get_mut(key) {
+        if let Some(entry) = self.database[item as usize].get_mut(key) {
             entry.kind.set_property(&entry.key, &entry.block, property);
         }
     }
@@ -121,13 +120,13 @@ impl Db {
         data: &Everything,
         sc: &mut ScopeContext,
     ) {
-        if let Some(entry) = self.database.get(&item).unwrap().get(key.as_str()) {
+        if let Some(entry) = self.database[item as usize].get(key.as_str()) {
             entry.kind.validate_call(&entry.key, &entry.block, key, block, data, sc);
         }
     }
 
     pub fn validate_use(&self, item: Item, key: &Token, block: &Block, data: &Everything) {
-        if let Some(entry) = self.database.get(&item).unwrap().get(key.as_str()) {
+        if let Some(entry) = self.database[item as usize].get(key.as_str()) {
             entry.kind.validate_use(&entry.key, &entry.block, data, key, block);
         }
     }
@@ -140,7 +139,7 @@ impl Db {
         property: &Token,
         caller: &str,
     ) {
-        if let Some(entry) = self.database.get(&item).unwrap().get(key.as_str()) {
+        if let Some(entry) = self.database[item as usize].get(key.as_str()) {
             entry.kind.validate_property_use(&entry.key, &entry.block, property, caller, data);
         }
     }
@@ -148,7 +147,7 @@ impl Db {
     /// TODO: Returns a Vec for now, should become an iterator.
     pub fn iter_itype(&self, itype: Item) -> Vec<(&Token, &Block, &dyn DbKind)> {
         let mut vec = Vec::new();
-        for entry in self.database.get(&itype).unwrap().values() {
+        for entry in self.database[itype as usize].values() {
             vec.push((&entry.key, &entry.block, &*entry.kind));
         }
         vec
@@ -157,7 +156,7 @@ impl Db {
     /// TODO: Returns a Vec for now, should become an iterator.
     pub fn iter_itype_flags(&self, itype: Item) -> Vec<&Token> {
         let mut vec = Vec::new();
-        for token in self.flags.get(&itype).unwrap().values() {
+        for token in self.flags[itype as usize].values() {
             vec.push(token);
         }
         vec
