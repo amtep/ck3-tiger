e64af2d99e7c64b000acfa76b7797d48e80cac3e
Richard Braakman <shuttlegit@gmail.com>
2023-08-22 20:13:58 +0300

    Version 0.8.6
    Hotfix for intro text

--- ck3-tiger/src/bin/ck3-tiger-auto.rs
+++ ck3-tiger/src/bin/ck3-tiger-auto.rs
@@ -43,8 +43,8 @@ fn inner_main() -> Result<()> {
         eprintln!("Failed to enable ANSI support for Windows10 users. Continuing anyway.")
     });
 
-    // LAST UPDATED CK3 VERSION 1.9.2.1
-    eprintln!("This validator was made for Crusader Kings version 1.9.2.1 (Lance).");
+    // LAST UPDATED CK3 VERSION 1.10.0
+    eprintln!("This validator was made for Crusader Kings version 1.10.0 (Quill).");
     eprintln!("If you are using a newer version of Crusader Kings, it may be inaccurate.");
     eprintln!("!! Currently it's inaccurate anyway because it's in beta state.");
 
