5ff083d7eb4c388caedd03dd39820f6232c17f21
Richard Braakman <shuttlegit@gmail.com>
2023-09-16 20:14:40 +0300

    CK3 update for 1.10.2

--- ck3-tiger/src/bin/ck3-tiger-auto.rs
+++ ck3-tiger/src/bin/ck3-tiger-auto.rs
@@ -43,8 +43,8 @@ fn inner_main() -> Result<()> {
         eprintln!("Failed to enable ANSI support for Windows10 users. Continuing anyway.")
     });
 
-    // LAST UPDATED CK3 VERSION 1.10.1
-    eprintln!("This validator was made for Crusader Kings version 1.10.1 (Quill).");
+    // LAST UPDATED CK3 VERSION 1.10.2
+    eprintln!("This validator was made for Crusader Kings version 1.10.2 (Quill).");
     eprintln!("If you are using a newer version of Crusader Kings, it may be inaccurate.");
     eprintln!("!! Currently it's inaccurate anyway because it's in beta state.");
 
