af30938038f5b265f0d9458c4a3598fe369ace7a
Michael Yang <admin@my4ng.dev>
2024-01-22 10:01:33 +1100

    Update benchmark and flamegraph

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -1,43 +1,58 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use std::path::PathBuf;
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use serde::Deserialize;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+use tiger_lib::{Everything, ModFile};
 
-use ck3_tiger::errors::{log_to, set_mod_root, set_vanilla_dir};
-use ck3_tiger::everything::Everything;
+static CONFIG_PATH: &str = "./benches/config.toml";
 
-fn apw_simple(c: &mut Criterion) {
-    let vanilla_dir = PathBuf::from("/home/gamer/CK3/..");
-    let mod_root = PathBuf::from("/home/gamer/Pdx/mod/A Primordial World");
+// Sample Config File:
+// vanilla_dir = "path"
+// modpaths = ["mod_path_1", "mod_path_2"]
+// sample_size = 50
 
-    set_vanilla_dir(vanilla_dir.clone());
-    set_mod_root(mod_root.clone());
-    log_to(Box::new(Vec::new()));
-
-    c.bench_function("apw", |b| {
-        b.iter(|| {
-            let mut everything = Everything::new(&vanilla_dir, &mod_root, Vec::new()).unwrap();
-            everything.load_all();
-            everything.validate_all();
-        })
-    });
+#[derive(Deserialize)]
+struct Config {
+    vanilla_dir: String,
+    modpaths: Vec<String>,
+    sample_size: Option<usize>,
 }
 
-fn pod_simple(c: &mut Criterion) {
-    let vanilla_dir = PathBuf::from("/home/gamer/CK3/..");
-    let mod_root = PathBuf::from("/home/dark/repo/github/pod/dev/devpod");
+fn bench_multiple(c: &mut Criterion) {
+    let content = fs::read_to_string(CONFIG_PATH).unwrap();
+    let config: Config = toml::from_str(&content).unwrap();
 
-    set_vanilla_dir(vanilla_dir.clone());
-    set_mod_root(mod_root.clone());
-    log_to(Box::new(Vec::new()));
+    let mut group = c.benchmark_group("benchmark");
+    group.sample_size(config.sample_size.unwrap_or(30));
+    for (index, modpath) in config.modpaths.iter().enumerate() {
+        let mut modpath = PathBuf::from(modpath);
+        modpath.push("descriptor.mod");
+        let modfile = ModFile::read(&modpath).unwrap();
+        group.bench_with_input(
+            BenchmarkId::new(
+                "mods",
+                format!("{}. {}", index + 1, modfile.display_name().unwrap_or_default()),
+            ),
+            &modfile,
+            |b, modfile_ref| {
+                b.iter_with_large_drop(|| bench_mod(&config.vanilla_dir, modfile_ref));
+            },
+        );
+    }
 
-    c.bench_function("pod", |b| {
-        b.iter(|| {
-            let mut everything = Everything::new(&vanilla_dir, &mod_root, Vec::new()).unwrap();
-            everything.load_all();
-            everything.validate_all();
-        })
-    });
+    group.finish();
 }
 
-criterion_group!(apw, apw_simple);
-criterion_group!(pod, pod_simple);
-criterion_main!(apw, pod);
+fn bench_mod(vanilla_dir: &str, modfile: &ModFile) -> Everything {
+    let mut everything =
+        Everything::new(Some(Path::new(vanilla_dir)), &modfile.modpath(), modfile.replace_paths())
+            .unwrap();
+    everything.load_all();
+    everything.validate_all();
+    everything
+}
+
+criterion_group!(benches, bench_multiple);
+criterion_main!(benches);
