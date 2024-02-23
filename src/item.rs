2ba0c1b43cf2e63dadc4419322548700f6087294
Richard Braakman <shuttlegit@gmail.com>
2024-02-23 07:33:35 +0200

    vic3: validate combat unit experience levels

--- src/item.rs
+++ src/item.rs
@@ -307,6 +307,7 @@ pub enum Item {
     #[cfg(feature = "vic3")] CanalType,
     #[cfg(feature = "vic3")] CharacterRole,
     #[cfg(feature = "vic3")] CombatUnit,
+    #[cfg(feature = "vic3")] CombatUnitExperienceLevel,
     #[cfg(feature = "vic3")] CommanderOrder,
     #[cfg(feature = "vic3")] CommanderRank,
     #[cfg(feature = "vic3")] CompanyType,
@@ -940,6 +941,8 @@ impl Item {
             Item::CharacterRole => "",
             #[cfg(feature = "vic3")]
             Item::CombatUnit => "common/combat_unit_types/",
+            #[cfg(feature = "vic3")]
+            Item::CombatUnitExperienceLevel => "common/combat_unit_experience_levels/",
             #[cfg(feature = "vic3")]
             Item::CommanderOrder => "common/commander_orders/",
             #[cfg(feature = "vic3")]
--- src/vic3/data/combat_units.rs
+++ src/vic3/data/combat_units.rs
@@ -4,6 +4,7 @@ use crate::db::{Db, DbKind};
 use crate::everything::Everything;
 use crate::game::GameFlags;
 use crate::item::{Item, ItemLoader};
+use crate::modif::{validate_modifs, ModifKinds};
 use crate::scopes::Scopes;
 use crate::token::Token;
 use crate::tooltipped::Tooltipped;
@@ -41,6 +42,36 @@ impl DbKind for OldCombatUnit {
                 validate_trigger(block, data, &mut sc, Tooltipped::No);
             });
             vd.field_item("texture", Item::File);
+        });
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CombatUnitExperienceLevel {}
+
+inventory::submit! {
+    ItemLoader::Normal(GameFlags::Vic3, Item::CombatUnitExperienceLevel, CombatUnitExperienceLevel::add)
+}
+
+impl CombatUnitExperienceLevel {
+    pub fn add(db: &mut Db, key: Token, block: Block) {
+        db.add(Item::CombatUnitExperienceLevel, key, block, Box::new(Self {}));
+    }
+}
+
+impl DbKind for CombatUnitExperienceLevel {
+    // This whole item type is undocumented.
+    fn validate(&self, key: &Token, block: &Block, data: &Everything) {
+        data.verify_exists(Item::Localization, key);
+
+        let mut vd = Validator::new(block, data);
+        // TODO: is there a requirement for levels to be consecutive?
+        vd.field_integer("level");
+        vd.field_item("icon", Item::File);
+        vd.field_integer("needed_experience");
+        vd.field_validated_block("unit_modifier", |block, data| {
+            let vd = Validator::new(block, data);
+            validate_modifs(block, data, ModifKinds::Unit, vd);
         });
     }
 }
