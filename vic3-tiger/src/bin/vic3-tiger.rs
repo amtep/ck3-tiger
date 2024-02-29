e62ece2f23fe9ec385df9d12cabe77d79a0a3e44
Richard Braakman <shuttlegit@gmail.com>
2024-02-29 02:03:06 +0200

    Version 0.8.12
    CK3 1.11.5
    Vic3 1.5.13
    
    Bugfixes
    * ck3: fix ancestor cycle detection
    * Fix --unused output
    * vic3: accept user-defined modifs, add missing modifs
    * vic3: update list of effects that accept script values
    
    Features
    * command-line argument --config for config file location
    * validate tutorials
    * ck3: validate climate.txt
    * ck3: validate lease contracts
    * ck3: validate fervor modifiers
    * ck3: validate artifact reforge blueprints
    * ck3: validate guest system, courtier guest managementa
    * ck3: validate ai war stances
    * ck3: validate cultural name equivalency lists
    * ck3: validate history wars
    * ck3: warn about conflicting religious doctrines
    * vic3: validate alerts
    * vic3: validate new combat units, combat unit experience levels
    * vic3: validate commander ranks and orders
    * vic3: validate company types
    * vic3: update ai_strategies validation
    * vic3: validate journal entry groups
    * gui: validate @icon! strings in gui texts
    * warn about files outside common that should be in common
    * performance: 12% faster

--- vic3-tiger/src/bin/vic3-tiger.rs
+++ vic3-tiger/src/bin/vic3-tiger.rs
@@ -54,8 +54,8 @@ fn main() -> Result<()> {
             .map_err(|_| eprintln!("Failed to enable ANSI support for Windows10 users. Continuing probably without colored output."));
     }
 
-    // LAST UPDATED VERSION VIC3 1.5.2
-    eprintln!("This validator was made for Victoria 3 version 1.5.12.");
+    // LAST UPDATED VERSION VIC3 1.5.13
+    eprintln!("This validator was made for Victoria 3 version 1.5.13.");
     eprintln!("If you are using a newer version of Victoria 3, it may be inaccurate.");
     eprintln!("!! Currently it's inaccurate anyway because it's in beta state.");
 
