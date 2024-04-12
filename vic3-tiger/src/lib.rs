ed9f8a70095dd142ad07c559bc1b12ebdeab78d6
Richard Braakman <shuttlegit@gmail.com>
2024-04-12 20:52:29 +0300

    Version 0.9.6
    CK3 1.12.4
    Vic3 1.6.2
    Imperator 2.0.4
    
    Fix false positives:
    * script value logic: track whether script values have been saved before being overwritten
    * gui: add `rightclicksound` property
    * ck3: `add_trait_xp` can take a scope as the trait
    * ck3: fix complex tooltip table for #TOOLTIP localization
    * ck3: `add_<lifestyle>_perk_points` can take a script value
    * imperator: allow empty values for several fields in provinces setup
    * imperator: allow `max_rivals` and `max_friends` for country scope
    * imperator: `barbarian_spawn_chance` is a province modifier
    
    Features:
    * Ability to suppress reports that are the same as in a previous run
    * `imperator-tiger-auto` executable for hopeful double-clicking

--- vic3-tiger/src/lib.rs
+++ vic3-tiger/src/lib.rs
@@ -3,7 +3,7 @@ use tiger_bin_shared::GameConsts;
 pub const GAME_CONSTS: &GameConsts = &GameConsts {
     name: "Victoria 3",
     name_short: "Vic3",
-    version: "1.6.0 (BLACKCURRANT)",
+    version: "1.6.2 (BLACKCURRANT)",
     dir: "steamapps/common/Victoria 3",
     app_id: "529340",
     signature_file: "game/events/titanic_events.txt",
