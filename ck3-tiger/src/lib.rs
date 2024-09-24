2c1d18cee35a553e3270d0093e84ab9c9d63b3a4
Richard Braakman <shuttlegit@gmail.com>
2024-09-24 19:14:36 +0300

    feat(ck3): update ck3 version

--- ck3-tiger/src/lib.rs
+++ ck3-tiger/src/lib.rs
@@ -3,7 +3,7 @@ use tiger_bin_shared::GameConsts;
 pub const GAME_CONSTS: &GameConsts = &GameConsts {
     name: "Crusader Kings 3",
     name_short: "CK3",
-    version: "1.12.3 (SCYTHE)",
+    version: "1.13.0.1 (Basileus)",
     app_id: 1_158_310,
     signature_file: "game/events/witch_events.txt",
     paradox_dir: "Crusader Kings III",
