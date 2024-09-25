7fde425eadc903531dd02de901d77f371847030b
Richard Braakman <shuttlegit@gmail.com>
2024-09-25 22:40:42 +0300

    feat(ck3): adapt flavorization validation to 1.13

--- src/ck3/data/flavorization.rs
+++ src/ck3/data/flavorization.rs
@@ -26,26 +26,54 @@ impl DbKind for Flavorization {
         data.verify_exists(Item::Localization, key);
 
         vd.req_field("type");
-        vd.field_choice("type", &["character", "title"]);
-        vd.field_choice("gender", &["male", "female"]);
-        vd.field_choice(
-            "special",
-            &["ruler_child", "holder", "queen_mother", "head_of_faith", "councillor"],
-        );
-        vd.field_choice("tier", &["empire", "kingdom", "duchy", "county", "barony"]);
+        vd.field_choice("type", &["character", "title", "domicile"]);
+        let flavor_type = block.get_field_value("type").map_or("none", Token::as_str);
+        if flavor_type == "character" {
+            vd.field_choice("gender", &["male", "female"]);
+            vd.field_choice(
+                "special",
+                &[
+                    "ruler_child",
+                    "holder",
+                    "queen_mother",
+                    "head_of_faith",
+                    "councillor",
+                    "domicile",
+                ],
+            );
+        } else {
+            vd.ban_field("gender", || "type = character");
+            vd.ban_field("special", || "type = character");
+        }
+        if flavor_type == "character" || flavor_type == "title" {
+            vd.field_choice("tier", &["empire", "kingdom", "duchy", "county", "barony", "none"]);
+        } else {
+            vd.ban_field("tier", || "type = character or title");
+        }
         vd.field_integer("priority");
+        vd.field_validated_block("flavorization_rules", |block, data| {
+            let mut vd = Validator::new(block, data);
+            vd.field_bool("faction");
+            vd.field_bool("only_independent");
+            vd.field_bool("spouse_takes_title");
+            vd.field_bool("only_holder");
+            vd.field_bool("top_liege");
+        });
+        vd.field_value("flag");
+        vd.field_list_items("governments", Item::GovernmentType);
+        vd.field_list_items("domicile_type", Item::DomicileType);
         vd.field_list_items("name_lists", Item::NameList);
         vd.field_list_items("heritages", Item::CultureHeritage);
-        vd.field_list_items("governments", Item::GovernmentType);
         vd.field_list_items("faiths", Item::Faith);
         vd.field_list_items("religions", Item::Religion);
-        vd.field_list_items("titles", Item::Title);
-        vd.field_list_items("de_jure_liege", Item::Title);
         vd.field_item("council_position", Item::CouncilPosition);
+        vd.field_list_items("de_jure_liege", Item::Title);
         vd.field_item("holding", Item::HoldingType);
-        vd.field_bool("top_liege");
-        vd.field_bool("only_holder");
-        vd.field_bool("only_independent");
-        vd.field_bool("faction");
+        vd.field_list_items("titles", Item::Title);
+
+        vd.advice_field("top_liege", "moved into flavorization_rules");
+        vd.advice_field("only_holder", "moved into flavorization_rules");
+        vd.advice_field("only_independent", "moved into flavorization_rules");
+        vd.advice_field("faction", "moved into flavorization_rules");
     }
 }
