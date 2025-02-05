026c4114c44d53c399551c4aff0b5505fece8e22
IhateTrains <IhateTrains@users.noreply.github.com>
2025-02-05 21:41:16 +0100

    Fix wrong I:R modifier being edited

--- src/imperator/tables/modifs.rs
+++ src/imperator/tables/modifs.rs
@@ -160,7 +160,7 @@ static MODIF_MAP: Lazy<TigerHashMap<Lowercase<'static>, ModifKinds>> = Lazy::new
 /// See `modifiers.log` from the game data dumps.
 /// A `modif` is my name for the things that modifiers modify.
 const MODIF_TABLE: &[(&str, ModifKinds)] = &[
-    ("happiness_for_wrong_religion_modifier", ModifKinds::Province.union(ModifKinds::Country)),
+    ("happiness_for_wrong_religion_modifier", ModifKinds::Province),
     ("local_happiness_for_wrong_religion_modifier", ModifKinds::Province),
     ("cultural_assimilation_speed_modifier", ModifKinds::Country),
     ("build_cost", ModifKinds::Country),
@@ -387,7 +387,7 @@ const MODIF_TABLE: &[(&str, ModifKinds)] = &[
     ("local_monthly_food_modifier", ModifKinds::Province.union(ModifKinds::State)),
     ("local_hostile_food_multiplier", ModifKinds::Province.union(ModifKinds::State)),
     ("pop_food_consumption", ModifKinds::Province.union(ModifKinds::State)),
-    ("monthly_character_experience", ModifKinds::Character),
+    ("monthly_character_experience", ModifKinds::Character.union(ModifKinds::Country)),
     ("monthly_character_experience_decay", ModifKinds::Character),
     ("monthly_conviction_for_head_of_family_party", ModifKinds::Character),
     ("local_base_trade_routes", ModifKinds::Province.union(ModifKinds::State)),
