62209f084821b9133203a50edb44ea9fc7af6267
Michael Yang <admin@my4ng.dev>
2025-03-23 12:11:04 +0000

    fix: binary name and dev profile panic

--- ck3-tiger/src/bin/ck3-tiger.rs
+++ ck3-tiger/src/bin/ck3-tiger.rs
@@ -3,5 +3,5 @@ use ck3_tiger::GAME_CONSTS;
 use tiger_bin_shared::tiger;
 
 fn main() -> Result<()> {
-    tiger(GAME_CONSTS, env!("CARGO_PKG_VERSION"))
+    tiger(GAME_CONSTS, env!("CARGO_PKG_VERSION"), env!("CARGO_PKG_NAME"))
 }
--- imperator-tiger/src/bin/imperator-tiger.rs
+++ imperator-tiger/src/bin/imperator-tiger.rs
@@ -3,5 +3,5 @@ use imperator_tiger::GAME_CONSTS;
 use tiger_bin_shared::tiger;
 
 fn main() -> Result<()> {
-    tiger(GAME_CONSTS, env!("CARGO_PKG_VERSION"))
+    tiger(GAME_CONSTS, env!("CARGO_PKG_VERSION"), env!("CARGO_PKG_NAME"))
 }
