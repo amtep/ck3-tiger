31c304b5187eb59bebc38f099288a55be04813ee
Richard Braakman <shuttlegit@gmail.com>
2025-04-06 09:35:39 +0300

    fix(hoi4): fix some warnings, including some bugs

--- src/effect_validation.rs
+++ src/effect_validation.rs
@@ -5,6 +5,7 @@ use crate::context::ScopeContext;
 use crate::desc::validate_desc;
 use crate::effect::{validate_effect, validate_effect_control};
 use crate::everything::Everything;
+#[cfg(feature = "jomini")]
 use crate::game::Game;
 use crate::item::Item;
 use crate::lowercase::Lowercase;
