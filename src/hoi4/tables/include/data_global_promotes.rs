a08332aea423acc9d8d3cbd7ae7b95dc3fa0205c
Richard Braakman <shuttlegit@gmail.com>
2025-04-07 12:28:35 +0300

    feat(hoi4): add global promotes

---
+++ src/hoi4/tables/include/data_global_promotes.rs
@@ -0,0 +1,23 @@
+// Unlike the other games, this list is created by hand for Hoi4.
+// It is the promotes that can be used in Scope context.
+&[
+    ("Ace", Args::Args(&[]), Hoi4(Ace)),
+    ("Character", Args::Args(&[]), Hoi4(Character)),
+    ("Combatant", Args::Args(&[]), Unknown),
+    ("Country", Args::Args(&[]), Hoi4(Country)),
+    ("From", Args::Args(&[]), Unknown),
+    ("FROM", Args::Args(&[]), Unknown),
+    ("IndustrialOrganisation", Args::Args(&[]), Hoi4(IndustrialOrg)),
+    ("Operation", Args::Args(&[]), Hoi4(Operation)),
+    ("Player", Args::Args(&[]), Hoi4(Country)),
+    ("Prev", Args::Args(&[]), Unknown),
+    ("PREV", Args::Args(&[]), Unknown),
+    ("PurchaseContract", Args::Args(&[]), Hoi4(PurchaseContract)),
+    ("Root", Args::Args(&[]), Unknown),
+    ("ROOT", Args::Args(&[]), Unknown),
+    ("SpecialProject", Args::Args(&[]), Hoi4(SpecialProject)),
+    ("State", Args::Args(&[]), Hoi4(State)),
+    ("THIS", Args::Args(&[]), Unknown),
+    ("This", Args::Args(&[]), Unknown),
+    ("Unit", Args::Args(&[]), Unknown),
+]
