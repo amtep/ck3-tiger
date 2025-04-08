8e1afa2179ae502d95e17c7b07d6338143fb121f
Richard Braakman <shuttlegit@gmail.com>
2025-04-08 03:34:40 +0300

    feat(hoi4): populate modif table

--- src/vic3/data/modifier_type_definitions.rs
+++ src/vic3/data/modifier_type_definitions.rs
@@ -7,7 +7,7 @@ use crate::modif::{verify_modif_exists, ModifKinds};
 use crate::report::Severity;
 use crate::token::Token;
 use crate::validator::Validator;
-use crate::vic3::tables::modifs::modif_loc;
+use crate::vic3::tables::modifs::modif_loc_vic3;
 
 /// Equivalent to CK3's `Item::ModifierFormat` in the `ck3::data::modif` module.
 
@@ -28,7 +28,7 @@ impl DbKind for ModifierTypeDefinition {
     fn validate(&self, key: &Token, block: &Block, data: &Everything) {
         let mut vd = Validator::new(block, data);
 
-        let (loca, loca_desc) = modif_loc(key, data);
+        let (loca, loca_desc) = modif_loc_vic3(key, data);
         data.verify_exists_implied(Item::Localization, &loca, key);
         data.verify_exists_implied(Item::Localization, &loca_desc, key);
 
