a798a4d69a497ac22d66361c33f94f79cae38273
Richard Braakman <shuttlegit@gmail.com>
2025-04-11 08:44:57 +0300

    feat(hoi4): validate static modifiers

---
+++ src/hoi4/data/modifiers.rs
@@ -0,0 +1,34 @@
+use crate::block::Block;
+use crate::db::{Db, DbKind};
+use crate::everything::Everything;
+use crate::game::GameFlags;
+use crate::item::{Item, ItemLoader};
+use crate::modif::{validate_modifs, ModifKinds};
+use crate::scopes::Scopes;
+use crate::token::Token;
+use crate::tooltipped::Tooltipped;
+use crate::validator::Validator;
+
+#[derive(Clone, Debug)]
+pub struct Modifier {}
+
+inventory::submit! {
+    ItemLoader::Normal(GameFlags::Hoi4, Item::Modifier, Modifier::add)
+}
+
+impl Modifier {
+    pub fn add(db: &mut Db, key: Token, block: Block) {
+        db.add(Item::Modifier, key, block, Box::new(Self {}));
+    }
+}
+
+impl DbKind for Modifier {
+    fn validate(&self, key: &Token, block: &Block, data: &Everything) {
+        let mut vd = Validator::new(block, data);
+
+        data.verify_exists(Item::Localization, key);
+
+        vd.field_trigger_full("valid_relation_trigger", Scopes::Country, Tooltipped::No);
+        validate_modifs(block, data, ModifKinds::all(), vd);
+    }
+}
