8178530c09476325dab2b02d2d6f4610938a953f
Richard Braakman <shuttlegit@gmail.com>
2025-05-02 18:44:01 +0300

    feat(ck3): bump ck3 version number

--- ck3-tiger/src/lib.rs
+++ ck3-tiger/src/lib.rs
@@ -3,7 +3,7 @@ use tiger_bin_shared::GameConsts;
 pub const GAME_CONSTS: &GameConsts = &GameConsts {
     name: "Crusader Kings 3",
     name_short: "CK3",
-    version: "1.15 (Crown)",
+    version: "1.16 (Chamfron)",
     app_id: 1_158_310,
     signature_file: "game/events/witch_events.txt",
     paradox_dir: "Crusader Kings III",
